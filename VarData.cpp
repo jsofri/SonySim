bf14ce37e217b52972450c3227f35c4fc219f552
ronyut <ronyut@gmail.com>
2019-12-31 16:17:14 +0200

    fixed a typo + added comments

--- VarData.cpp
+++ VarData.cpp
@@ -1,12 +1,30 @@
-//
-// Created by rony on 12/27/19.
-//
+/**
+ * VarData class is essentially a struct that hold 3 things:
+ * (1) reference to simulator (a path, e.g. `engines/engine/rpm`) 
+ * (2) the value (floating point number)
+ * (3) the updater of the variable: Simulator / Client / No one.
+ * 
+ * Structs like this are held in the Symbol Table.
+ * 
+ * @date 12/21/19
+ * @author Rony Utevsky
+ **/
 
 #include "GeneralData.h"
 #include "VarData.h"
 
-using namespace std;
+/**
+ * Constructor
+ *
+ * @param referece the reference
+ * @param value the value
+ * @param updater the updater
+ **/
+VarData :: VarData(string reference, float value, int updater): reference(reference), value(value), updater(updater) {}
 
-VarData :: VarData(string reference, float value, int update): reference(reference), value(value), updater(updater) {}
-
+/**
+ * Constructor
+ *
+ * @param value the value
+ **/
 VarData :: VarData(float value): value(value) {}
