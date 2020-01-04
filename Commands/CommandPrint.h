271f8a7ba39fa461da3c8a0e71845f4cee9cc0d9
ronyut <ronyut@gmail.com>
2020-01-04 20:42:02 +0200

    added comments

--- Commands/CommandPrint.h
+++ Commands/CommandPrint.h
@@ -1,15 +1,15 @@
-//
-// Created by rony on 12/26/19.
-//
+/**
+ * this class is for a print command.
+ *
+ * @date 12/26/19
+ * @author Rony 
+ */
 
 #ifndef COMMANDPRINT_H
 #define COMMANDPRINT_H
 
 #include "Command.h"
 
-/**
- * Print Command
- */
 class CommandPrint: public Command {
     int execute(int);
 };
