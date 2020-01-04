bff1cd11d687cb8f00d34f1b4bd2cebb3a8bbf38
ronyut <ronyut@gmail.com>
2020-01-04 17:46:45 +0200

    added comments

--- SymbolTable.h
+++ SymbolTable.h
@@ -16,16 +16,67 @@
 
 class SymbolTable {
 public:
+    /**
+     * adds new map to the list.
+     */
     void    newTable();
+    
+    /**
+     * adds new table to the list.
+     *
+     * @param new_map to enter to beginning of list
+     */
     void    newTable(unordered_map<string, VarData> &);
+    
+    /**
+     * delete first map from the list, i.e. delete the innermost scope's list
+     */
     void    deleteTable();
+    
+    /**
+     * check if a key exist in the symbol table.
+     *
+     * @param str - key to look for in symbol table
+     * @return boolean
+     */
     bool    exists(string &);
+    
+    /**
+     * return value in one of the maps.
+     *
+     * @param str key to look for in maps
+     * @return value attached to the key in the closest to the beginning
+     * @throw char * if key not found
+     */
     VarData& get(string);
+    
+    /**
+     * enter new value to the first map.
+     *
+     * @param key string - name of variable
+     * @param value - VarData
+     */
     void    insert(string &, VarData &);
+    
+    /**
+     * update field "value" in the var data node that's represented by key.
+     * @param key string key in symbolTable
+     * @param value in varData of the specific key
+     */
     void    update(string &, float);
+    
+    /**
+     * Constructor
+     */
     SymbolTable();
+    
+    /**
+     * Destructor
+     */
     ~SymbolTable() = default;
+    
 private:
+    // a list that holds all the maps (each map represents a scope in the code - currently maximum 2 maps)
     list<unordered_map<string, VarData>> _symbol_tables;
     mutex _locker;
 };
